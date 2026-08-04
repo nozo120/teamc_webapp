3fb283ff2f6e67e7010f20a2847fc8a16c87fa11
Rina Miyazawa <r.miyazawa@akane.waseda.jp>
2026-08-04 16:18:35 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserInfo from './pages/Miyazawa/ss';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <UserInfo />
       </header>
     </div>
   );
