7417537e57e8a9f34f585e96f5b62e0f1d1382a6
ST_IDEAPAD\sota0 <sota0921bb@gmail.com>
2026-08-05 11:34:38 +0900

    DBのデータを格納するtypeを作成

--- frontend/src/pages/Hayashi/api/userApi.ts
+++ frontend/src/pages/Hayashi/api/userApi.ts
@@ -1,5 +1,5 @@
 // src/api/userApi.ts
-import { User } from '../User';
+import { user } from '../../../user';
 
 const API_URL = 'http://localhost:3010/users';
 
@@ -7,7 +7,7 @@ const API_URL = 'http://localhost:3010/users';
  * DB (json-server) からユーザー情報の一覧を取得する関数
  * @returns Promise<User[]> ユーザーオブジェクトの配列
  */
-export async function getUsers(): Promise<User[]> {
+export async function getUsers(): Promise<user[]> {
     const response = await fetch(API_URL);
 
     if (!response.ok) {
@@ -15,6 +15,6 @@ export async function getUsers(): Promise<User[]> {
     }
 
     // 取得した JSON データを User のリストとして返却
-    const users: User[] = await response.json();
+    const users: user[] = await response.json();
     return users;
 }
