1dbb707471b9cdb18f470d69275139066420562b
RyogaMorita <moritarixyouga@gmail.com>
2026-08-05 16:46:51 +0900

    支払い画面のルートを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { getMyUserId } from './utils/myUserId';
 // TransferScreen は default export なので { } は付けない
 import TransferScreen from './pages/Morita/TransferScreen';
 import TransferComplete from './pages/Morita/TransferComplete';
+import PaymentScreen from './pages/Morita/PaymentScreen';
 
 // TODO: ログイン機能ができたら、自分のIDはログイン情報から取得する
 const MY_USER_ID = getMyUserId();
@@ -45,6 +46,15 @@ function App() {
         />
         {/* TODO: 送金完了画面ができたら element を差し替える */}
         <Route path={PATHS.COMPLETE} element={<TransferComplete />} />
+        {/* 請求リンクから開く支払い画面 */}
+        <Route
+          path="/payment"
+          element={
+            myBalance === null
+              ? <div>読み込み中...</div>
+              : <PaymentScreen maxAmount={myBalance} senderId={MY_USER_ID} />
+          }
+        />
         {/* 定義していないURLはホームに戻す */}
         <Route path="*" element={<Navigate to={PATHS.HOME} replace />} />
       </Routes>
