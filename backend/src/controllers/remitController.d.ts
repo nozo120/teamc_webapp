5b27dd8bc5c8e7ed5629da82d59841582cc14df6
Rina Miyazawa <r.miyazawa@akane.waseda.jp>
2026-08-06 13:41:48 +0900

    AA

---
+++ backend/src/controllers/remitController.d.ts
@@ -0,0 +1,3 @@
+import type { Request, Response } from "express";
+export declare const handleRemit: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+//# sourceMappingURL=remitController.d.ts.map
---
+++ backend/src/services/remitService.d.ts
@@ -0,0 +1,2 @@
+export declare function dealing(senderId: number, receiverId: number, amount: number, message?: string): Promise<string>;
+//# sourceMappingURL=remitService.d.ts.map
