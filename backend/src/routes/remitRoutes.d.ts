0bb9e9c68271d96d2688c3f3e6d81f8c05c52221
Rina Miyazawa <r.miyazawa@akane.waseda.jp>
2026-08-06 13:50:02 +0900

    修正した

--- backend/src/routes/remitRoutes.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=remitRoutes.d.ts.map
--- backend/src/services/remitService.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function dealing(senderId: number, receiverId: number, amount: number, message?: string): Promise<string>;
-//# sourceMappingURL=remitService.d.ts.map
